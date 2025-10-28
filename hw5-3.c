594d6afc365d8a0b47bf99945f80c443c2887d1e
am21006 <167287319+am21006@users.noreply.github.com>
2025-10-28 11:57:52 +0900

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+    int a, b;
+    printf("Please enter two different numbers\n");
+    if (scanf("%d %d", &a, &b) != 2) return 0;
+
+    while (a == b) {
+        printf("Same, please enter two different numbers\n");
+        if (scanf("%d %d", &a, &b) != 2) return 0;
+    }
+
+    printf("You entered %d and %d\n", a, b);
+    return 0;
+}
